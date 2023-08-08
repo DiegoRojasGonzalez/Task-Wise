ece0620aee1d590e046edb550132f1d858c94103
Diego Ignacio Rojas Gonzalez <gonzalez.rojas.ignacio.diego@gmail.com>
2023-08-07 20:53:44 -0400

    feat : add enter key fuction focus and new task function

--- src/app/task-input/task-input.component.ts
+++ src/app/task-input/task-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-task-input',
@@ -6,13 +6,28 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./task-input.component.css']
 })
 export class TaskInputComponent {
+  @ViewChild('taskInput') taskInput!: ElementRef;
+
   @Output() taskAdded = new EventEmitter<string>();
   inputText: string = '';
+
+  ngAfterViewInit() {
+    this.taskInput.nativeElement.focus();
+  }
 
   onAddTask() {
     if (this.inputText.trim() !== '') {
       this.taskAdded.emit(this.inputText);
     }
     this.inputText = '';
+    this.taskInput.nativeElement.focus();
+  }
+
+  @HostListener('window:keydown.enter', ['$event'])
+  handleEnterKeyPress(event: KeyboardEvent) {
+    if (event.target !== this.taskInput.nativeElement) {
+      event.preventDefault();
+      this.taskInput.nativeElement.focus();
+    }
   }
 }
