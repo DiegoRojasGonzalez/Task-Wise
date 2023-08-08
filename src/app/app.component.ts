6c5624fd68af3145db42b53aa8fe528adcc41a35
Diego Ignacio Rojas Gonzalez <gonzalez.rojas.ignacio.diego@gmail.com>
2023-08-07 20:29:22 -0400

    feat: add remove task function
    - add save in local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Task-Wise';
   tasks: string[] = [];
 
+  ngOnInit() {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      this.tasks = JSON.parse(storedTasks);
+    }
+  }
+
   addTaskToList(taskName: string) {
     this.tasks.push(taskName);
+    this.updateLocalStorage();
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+    this.updateLocalStorage();
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
