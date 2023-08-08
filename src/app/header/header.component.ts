4200511e70a790b518bf9a835411e51b271a2383
Diego Ignacio Rojas Gonzalez <gonzalez.rojas.ignacio.diego@gmail.com>
2023-08-08 17:04:12 -0400

    style: add tasklist desing
    - Tasklist desing and app component desing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,15 +8,30 @@ import { Component, Renderer2 } from '@angular/core';
 export class HeaderComponent {
   darkMode = false;
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2) {
+    // Recuperar el modo actual desde el Local Storage al cargar
+    const storedDarkMode = localStorage.getItem('darkMode');
+    if (storedDarkMode) {
+      this.darkMode = JSON.parse(storedDarkMode);
+    }
+
+    // Aplicar el modo guardado
+    if (this.darkMode) {
+      this.renderer.addClass(document.body, 'dark-mode');
+    }
+  }
 
   toggleDarkMode() {
     this.darkMode = !this.darkMode;
     console.log(this.darkMode);
+
     if (this.darkMode) {
       this.renderer.addClass(document.body, 'dark-mode');
     } else {
       this.renderer.removeClass(document.body, 'dark-mode');
     }
+
+    // Guardar el modo actual en el Local Storage
+    localStorage.setItem('darkMode', JSON.stringify(this.darkMode));
   }
 }
